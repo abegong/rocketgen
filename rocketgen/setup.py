10750b37b970c98170ac0548b5bc210eca88d82a
Abe Gong <abegong@gmail.com>
2018-10-07 14:12:51 -0700

    Create rocketgen module

---
+++ rocketgen/setup.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="rocketgen",
+    packages=["rocketgen"],
+    version="0.0.1",
+    description="Infinite variations on rockets",
+    author="Abe Gong",
+    license="Apache 2.0",
+)
